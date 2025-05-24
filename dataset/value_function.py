f4b7de9a1962a2fde6f5705aff1360a35c2fb743
MALTE <maltenygren01@gmail.com>
2025-05-23 21:12:11 -0700

    highest

--- dataset/value_function.py
+++ dataset/value_function.py
@@ -51,7 +51,7 @@ def get_V(env, dynamics, grid, times, convergence_threshold=CONVERGENCE_THRESHOL
     """
     # Configure JAX memory management
     jax.config.update('jax_platform_name', 'gpu')
-    jax.config.update('jax_default_matmul_precision', jax.lax.Precision.HIGHEST)
+    jax.config.update('jax_default_matmul_precision', 'highest')
         
     # Extract x, y coordinates from the grid states
     x = grid.states[..., 0]
