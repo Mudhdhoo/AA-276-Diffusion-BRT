391e4a4654110855af71f7e2168080aed4123a24
MALTE <maltenygren01@gmail.com>
2025-06-06 20:03:33 -0700

    NEW STATS 6000

--- scripts/compute_stats.py
+++ scripts/compute_stats.py
@@ -1,7 +1,12 @@
+import os
+import sys
+
+#add path to python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from dataset.BRTDataset import BRTDataset
 import numpy as np
 
-path = "../6000_density_weighted"
+path = "6000_density_weighted"
 
 dataset = BRTDataset(path, split="train", return_value_function=False)
 
